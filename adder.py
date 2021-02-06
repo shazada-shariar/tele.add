a8ca075cb17a6cea44d27fb2a41cc0dbfeda5c62
SHAZADA §HARIAR <68187675+shazada-shariar@users.noreply.github.com>
2021-02-06 21:03:17 +0430

    Update adder.py

--- adder.py
+++ adder.py
@@ -15,25 +15,20 @@ re="\033[1;31m"
 gr="\033[1;32m"
 cy="\033[1;36m"
 
-print (re+"╔══╦╗╔╦══╦═╦══╦══╦═╗")
-print (gr+"║══╣╚╝║╔╗║╬╠║║╣╔╗║╬║")
-print (cy+"╠══║╔╗║╠╣║╗╬║║╣╠╣║╗╣")
-print (re+"╚══╩╝╚╩╝╚╩╩╩══╩╝╚╩╩╝")
-print (gr+" =============================== ")
-print (re+" =   Sultan      =    Shariar  = ")
-print (gr+" =   KING        =    AFG      = ")
-print (re+" =============================== ")
+print (re+"╔╦╗┌─┐┬  ┌─┐╔═╗  ╔═╗┌┬┐┌┬┐┌─┐┬─┐")
+print (gr+" ║ ├┤ │  ├┤ ║ ╦  ╠═╣ ││ ││├┤ ├┬┘")
+print (re+" ╩ └─┘┴─┘└─┘╚═╝  ╩ ╩─┴┘─┴┘└─┘┴└─")
 
-print (cy+" version : 2.0.0 ")
-print (re+" This script Careted by **Sultan Shariar** ")
-print (cy+" https://t.me/httpTremaxcommands ")
+print (cy+"version : 1.01")
+print (cy+"Make sure you Subscribed Termux Professor On Youtube")
+print (cy+"www.youtube.com/c/TermuxProfessorYT")
 
 print (re+"NOTE :")
-print ("1. >>Telegram only allow to add 200 members in group by one user<<.")
-print ("2. You can Use Api Telegram accounts for add more members.")
-print ("3. *Add only 50 members in group each time otherwise you will get flood error*.")
-print ("4. Then wait for ((15-30)) miniute then add members again.")
-print ("5. Make sure you enable Add User Permission in your group  [Sultan Shariar]")
+print ("1. Telegram only allow to add 200 members in group by one user.")
+print ("2. You can Use multiple Telegram accounts for add more members.")
+print ("3. Add only 50 members in group each time otherwise you will get flood error.")
+print ("4. Then wait for 15-30 miniute then add members again.")
+print ("5. Make sure you enable Add User Permission in your group")
 
 cpass = configparser.RawConfigParser()
 cpass.read('config.data')
